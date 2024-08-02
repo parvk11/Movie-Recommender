834df39b9d35a1e6746a24a34cb95af7acaefc7f
parvk11 <kparv01@gmail.com>
2024-08-02 17:10:41 -0400

    added memory constraint for tf

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,17 @@ model = joblib.load("movie-recommender.pkl")
 class UserInput(BaseModel):
     movie_w_rating: dict
 
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+
 
 X = model["X"]
 Y = model["Y"]
